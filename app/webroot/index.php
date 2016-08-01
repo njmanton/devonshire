246ea27f431f457c39e092d08bd10192421433f9
Nick Manton <nick@mantonbradbury.com>
2016-08-02 00:53:29 +0100

    Update config for Mac dev

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -56,7 +56,7 @@ if (!defined('APP_DIR')) {
  *
  * Leaving this constant undefined will result in it being defined in Cake/bootstrap.php
  */
-	define('CAKE_CORE_INCLUDE_PATH', 'C:\Users\Nick\Dropbox\cakelib' . DS . 'lib');
+	define('CAKE_CORE_INCLUDE_PATH', '/Users/nickm/Dropbox/cakelib' . DS . 'lib');
 
 /**
  * Editing below this line should NOT be necessary.
